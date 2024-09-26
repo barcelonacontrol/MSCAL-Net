1b64bc96a1313398571a43d09c5a0b6ce10835ac
whf <1292597139@qq.com>
2024-09-26 23:41:08 +0800

    send

---
+++ codemscalnet/code/networks/critic.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+
+
+class Discriminator(torch.nn.Module):
+    def __init__(self, in_channels=1, dim=64, out_conv_channels=1):
+        super(Discriminator, self).__init__()
+        conv1_channels = 16
+        conv2_channels = 96
+        conv3_channels = 192
+        self.out_conv_channels = out_conv_channels
+
+        self.conv11 = nn.Sequential(
+            nn.Conv3d(
+                in_channels=in_channels, out_channels=conv1_channels, kernel_size=1,
+                stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm3d(conv1_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv12 = nn.Sequential(
+            nn.Conv3d(
+                in_channels=in_channels, out_channels=conv1_channels, kernel_size=1,
+                stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm3d(conv1_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv13 = nn.Sequential(
+            nn.Conv3d(
+                in_channels=in_channels, out_channels=conv1_channels, kernel_size=1,
+                stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm3d(conv1_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv3d(
+                in_channels=3 * conv1_channels, out_channels=conv2_channels, kernel_size=1,
+                stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm3d(conv2_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv3d(
+                in_channels=conv2_channels, out_channels=conv3_channels, kernel_size=1,
+                stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm3d(conv3_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv3d(
+                in_channels=conv3_channels, out_channels=out_conv_channels, kernel_size=1,
+                stride=1, padding=0, bias=False
+            ),
+            nn.BatchNorm3d(out_conv_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+
+    def forward(self, map1, map2, image):
+        batch_size = map1.shape[0]
+        map_feature1 = self.conv11(map1)
+        map_feature2 = self.conv12(map2)
+        image_feature = self.conv13(image)
+        x = torch.cat([map_feature1, map_feature2, image_feature],dim=1)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        return x
+
+if __name__ == '__main__':
+    # compute FLOPS & PARAMETERS
+    from ptflops import get_model_complexity_info
+    model = Discriminator()
+    with torch.cuda.device(0):
+      macs, params = get_model_complexity_info(model, (1, 112, 112, 80), as_strings=True,
+                                               print_per_layer_stat=True, verbose=True)
+      print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+      print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+    with torch.cuda.device(0):
+      macs, params = get_model_complexity_info(model, (1, 96, 96, 96), as_strings=True,
+                                               print_per_layer_stat=True, verbose=True)
+      print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+      print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+    import ipdb; ipdb.set_trace()
